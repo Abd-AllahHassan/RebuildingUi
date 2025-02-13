dcabe3c2af7e8d371620a99e42e08edca84240ad
Abd-AllahHassan <aba252004@gmail.com>
2025-02-14 01:45:38 +0200

    update

--- js/main.js
+++ js/main.js
@@ -2,18 +2,20 @@ let header = document.querySelector("header");
 let menuIcon = document.querySelector("#menu-icon");
 let navbar = document.querySelector(".navbar");
 
+// Toggle mobile menu
 menuIcon.onclick = () => {
   navbar.classList.toggle("active");
   navbar.classList.toggle("hidden");
-}
+};
 
+// Hide mobile menu on scroll
 window.onscroll = () => {
   navbar.classList.remove("active");
   navbar.classList.add("hidden");
-}
+};
 
-window.addEventListener('scroll', () => {
+// Add shadow and background color to header on scroll
+window.addEventListener("scroll", () => {
   header.classList.toggle("shadow", window.scrollY > 0);
   header.classList.toggle("bg-bg", window.scrollY > 0);
 });
-
